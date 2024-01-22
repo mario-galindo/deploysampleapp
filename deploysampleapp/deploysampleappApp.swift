2095f2ed895c15dcdc9e8a19316168eaebf592cb
mario-galindo <mgalindo9410@gmail.com>
2024-01-22 01:04:45 -0600

    Initial Commit

---
+++ deploysampleapp/deploysampleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  deploysampleappApp.swift
+//  deploysampleapp
+//
+//  Created by Mario Galindo on 22/1/24.
+//
+
+import SwiftUI
+
+@main
+struct deploysampleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
